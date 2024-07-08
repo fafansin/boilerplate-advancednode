1302a4f38ad11a1cf0c2c0d01f66b0343061ab1c
Laviel Dela Cruz <fafansin@gmail.com>
2024-07-08 08:56:00 +0000

    feat: configured pug view engine, express-session and passport

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const { cookie } = require('express/lib/response.js');
+const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
 
@@ -10,9 +13,22 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave:true,
+  saveUninitialized: true,
+  cookie: {secure: false }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.route('/').get((req, res) => {
-
+  res.render('index', {title:'Hello', message:'Please log in'});
 });
 
 const PORT = process.env.PORT || 3000;
