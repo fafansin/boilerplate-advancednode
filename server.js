c5971d284a6f9c1d9df8102dfc47cde62fd07ac0
Laviel Dela Cruz <fafansin@gmail.com>
2024-07-11 15:42:36 +0000

    feat: passing username from server to  profile.pug and handle display

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ myDB(async client => {
   app
     .route('/profile')
     .get(ensureAuthenticated, (req,res) => {
-      res.render('/profile');
+      res.render('/profile', {username:req.user.username});
     })
   
 })
