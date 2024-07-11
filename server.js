cdbe954b4165014281df0626d7035f96e4880cde
Laviel Dela Cruz <fafansin@gmail.com>
2024-07-11 10:28:45 +0000

    feat: established connection to mongodb

--- server.js
+++ server.js
@@ -27,20 +27,31 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.serializeUser((user, done) => {
-  done(null, user._id);
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
+
+  app.route('/').get((req, res) => {
+    res.render('index', {
+      title:'Connected to Database', 
+      message:'Please log in'
+    });
+  });
+
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  })
+  
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id)}, (err, doc) => {
+      done(null, doc);
+    })
+  })
+
+
 })
 
-passport.deserializeUser((id, done) => {
-  // myDataBase.findOne({ _id: new ObjectID(id)}, (err, doc) => {
-  //   done(null, null);
-  // })
-  done(null, null);
-})
 
-app.route('/').get((req, res) => {
-  res.render('index', {title:'Hello', message:'Please log in'});
-});
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
