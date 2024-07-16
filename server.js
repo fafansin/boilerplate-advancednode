e4162cb0e0f48ad8731a7f3be91334b6cc9e920e
Laviel Dela Cruz <fafansin@gmail.com>
2024-07-16 08:21:23 +0000

    feat: passing username variable from server and display it on profile view page

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ myDB(async client => {
   
   app.route('/profile')
     .get(ensureAuthenticated, (req, res) => {
-      res.render('profile');
+      res.render('profile', {username:req.user.username});
     })
 
   app.route('/')
